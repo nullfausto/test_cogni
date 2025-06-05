d91663d0976f0c02bebd1013c8e0975a533bbc90
nullfausto <116468064+nullfausto@users.noreply.github.com>
2025-06-05 09:19:37 -0600

    Test application class

---
+++ profile-logging-demo/src/test/ProfileLoggingDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.profile_logging_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfileLoggingDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
