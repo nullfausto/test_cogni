f7e9211eb6c23689519d15f8be7b06a3764d3d1d
nullfausto <116468064+nullfausto@users.noreply.github.com>
2025-06-04 19:47:55 -0600

    Adding resources for sample data in DB

---
+++ task-management-api/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO tasks (title, description, completed, created_date) VALUES
+('Publish diagrams', 'Publish the completed diagrams to the collaboration hub', false, '2025-06-01 09:00:00'),
+('Finish project documentation', 'Complete the documentation on Payments Workaroung', true, '2025-06-01 10:30:10'),
+('Dry-run meeting', 'Meetings for testing the final prodcuts', false, '2025-06-02 14:15:28'),
+('Program Peer Review', 'Check the workaroung code for class', true, '2025-06-03 16:45:41'),
+('Ticket SJ-11', 'Ticket was arised due to a bug with the Payments service', false, '2025-06-04 05:20:54');
